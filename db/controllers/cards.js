fd54d55479d9be010d17dc7affbb6e054dbade74
Won Jin Chung <chungw51993@gmail.com>
2017-03-13 18:16:06 -0500

    Added update function to card controlelr

--- db/controllers/cards.js
+++ db/controllers/cards.js
@@ -8,5 +8,9 @@ var insertOne = function(card, cb) {
   CardModel.create(card, cb);
 };
 
+var updateCard = function(title, message, cb) {
+  CardModel.update({title: title}, {$set: {card: message}}, cb);
+};
+
 exports.findAllBy = findAllBy;
 exports.insertOne = insertOne;
