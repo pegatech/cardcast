e83443f16de12fa93dd0fbeccea3f0304215e9ad
Jerry Krusinski <jkruzr@gmail.com>
2017-03-14 13:23:34 -0500

    fixed typos

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -3,11 +3,11 @@ var path = require('path');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../clients/sender/index.html'));
+  res.sendFile(path.join(__dirname, '../../clients/sender/index.html'));
 });
 
 router.get('/receiver', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../clients/receiver/index.html'));
+  res.sendFile(path.join(__dirname, '../../clients/receiver/index.html'));
 });
 
 module.exports = router;
