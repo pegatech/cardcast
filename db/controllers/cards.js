29389596c70b3561ce35df10793f1a7074b93f42
Won Jin Chung <chungw51993@gmail.com>
2017-03-14 10:42:42 -0500

    Added remove function in card controller

--- db/controllers/cards.js
+++ db/controllers/cards.js
@@ -12,5 +12,9 @@ var updateCard = function(title, message, cb) {
   CardModel.update({title: title}, {$set: {card: message}}, cb);
 };
 
+var deleteCard = function(title, cb) {
+  CardModel.remove({title: title}, cb);
+};
+
 exports.findAllBy = findAllBy;
 exports.insertOne = insertOne;
