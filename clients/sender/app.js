e3747b0301fbffcac9201312c489955a6ee5b519
Jerry Krusinski <jkruzr@gmail.com>
2017-03-18 14:30:43 -0500

    made a bunch of bug fixes

--- clients/sender/app.js
+++ clients/sender/app.js
@@ -13,6 +13,81 @@ angular.module('cardcast', [
     return Auth.isAuth();
   };
 
+  var connect = function() {
+    if (!window.session) {
+      var applicationID = DEV_APP_ID;
+
+      window.namespace = 'urn:x-cast:pegatech.card.cast';
+      window.isCasting = false;
+      window.session = null;
+
+      var onInitSuccess = function() {
+        console.log('Successful initialization');
+      };
+
+      var onError = function(message) {
+        console.log('onError: ' + JSON.stringify(message));
+      };
+
+      var onSuccess = function(message) {
+        console.log('onSuccess: ' + message);
+      };
+
+      var onStopAppSuccess = function() {
+        console.log('Successful stop');
+      };
+
+      var sessionUpdateListener = function(isAlive) {
+        var message = isAlive ? 'Session Updated' : 'Session Removed';
+        message += ': ' + session.sessionId;
+        console.log(message);
+        if (!isAlive) {
+          session = null;
+        }
+      };
+
+      var receiverMessage = function(namespace, message) {
+        isCasting = JSON.parse(message);
+        console.log('function entered');
+        console.log('receiverMessage: ' + namespace + ', ' + message);
+      };
+
+      var receiverListener = function(event) {
+        if (event === 'available') {
+          console.log('receiver found');
+        } else {
+          console.log('receiver list empty');
+        }
+      };
+
+      window.sessionListener = function (currentSession) {
+        console.log('New session ID: ' + currentSession.sessionId);
+        session = currentSession;
+        session.addUpdateListener(sessionUpdateListener);
+        session.addMessageListener(namespace, receiverMessage);
+      };
+
+
+      var stopApp = function() {
+        session.stop(onStopAppSuccess, onError);
+      };
+
+
+      var initialize = function() {
+        if (!chrome.cast || !chrome.cast.isAvailable) {
+          setTimeout(initialize, 1000);
+        } else {
+          var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+          var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
+
+          chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+        }
+      };
+
+      initialize();
+    }
+  };
+
   $routeProvider
     .when('/login', {
       templateUrl: '/sender/controllers/auth/login.html',
@@ -26,25 +101,28 @@ angular.module('cardcast', [
       templateUrl: '/sender/controllers/main/main.html',
       controller: 'MainCtrl',
       resolve: {
-        user: authorize
+        user: authorize,
+        connect: connect
       }
     })
     .when('/new', {
       templateUrl: '/sender/controllers/new/new.html',
       controller: 'NewCtrl',
       resolve: {
-        user: authorize
+        user: authorize,
+        connect: connect
       }
     })
     .when('/edit/:id', {
       templateUrl: '/sender/controllers/edit/edit.html',
       controller: 'EditCtrl',
       resolve: {
-        user: authorize
+        user: authorize,
+        connect: connect
       }
     })
     .otherwise({
-      redirectTo: '/cards'
+      redirectTo: '/login'
     });
 })
 
