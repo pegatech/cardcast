dba561794d490b88bb52bd686c54e361a59302ae
Won Jin Chung <chungw51993@gmail.com>
2017-03-19 12:21:26 -0500

    Added some comments

--- db/controllers/users.js
+++ db/controllers/users.js
@@ -1,17 +1,21 @@
 var UserModel = require('../models/users');
 
+// Function to find a user info from the database using their username
 exports.findUser = function(username) {
   return UserModel.findOne({ username: username });
 };
 
+// Function to find a user from the database using their id
 exports.findUserById = function(id) {
   return UserModel.findById(id);
 };
 
+// Function to insert a new user into the database
 exports.insertUser = function(user) {
   return UserModel.create(user);
 };
 
+// Function to update user info in the database
 exports.updateUser = function(id, updates) {
   return UserModel.findByIdAndUpdate(id, updates);
 };
--- server/routes/clients.js
+++ server/routes/clients.js
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
+// Handle get request to '/' by sending them the sender index page
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../../clients/sender/index.html'));
 });
 
+// Handle get request to '/receiver' by sending them the receiver index page
 router.get('/receiver', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../../clients/receiver/index.html'));
 });
